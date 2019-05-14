e72ccea3398590e1bed87813a7cbc360e0bd78ad
Taylor Mosier <taymosier@gmail.com>
2019-05-14 07:16:35 -0400

    Updates

--- precache-manifest.f2455954e9b2494999926d4098154a16.js
+++ precache-manifest.f2455954e9b2494999926d4098154a16.js
@@ -8,7 +8,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "e9877d9ff3b0ca6130db",
+    "revision": "32cc6d9768916647dc37",
     "url": "/portfolio/static/js/main.1d4b5fa5.chunk.js"
   },
   {
@@ -16,15 +16,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/2.7b4f8604.chunk.js"
   },
   {
-    "revision": "e9877d9ff3b0ca6130db",
-    "url": "/portfolio/static/css/main.d58c646d.chunk.css"
+    "revision": "32cc6d9768916647dc37",
+    "url": "/portfolio/static/css/main.ad377070.chunk.css"
   },
   {
     "revision": "72164b9f23f68b3b8bc0",
     "url": "/portfolio/static/css/2.a5a642e5.chunk.css"
   },
   {
-    "revision": "d7e344ec98174c06bd7dea1bb0e2cc86",
+    "revision": "047a34c8d31f7dc016bca898b7a8bf99",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7a3b7e01f315f306cc7a7c60f01fc68d.js"
+  "/portfolio/precache-manifest.f2455954e9b2494999926d4098154a16.js"
 );
 
 workbox.clientsClaim();
